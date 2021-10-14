b43928a422f5776d811c7e1d1664061fef24e45b
NeonSpork <leiknes@gmail.com>
2021-10-14 10:36:49 +0200

    refactor: add routing for home page and admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
+  { path: '*', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appwrite } from 'appwrite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appwrite-blog-example';
+  appwrite = new Appwrite();
+
+  constructor() {
+    this.appwrite
+      .setEndpoint('http://localhost/v1') // Appwrite Endpoint
+      // **********************
+      // FIXME Make sure you put your OWN project ID here!
+      .setProject('6167e645d1969');
+      // **********************
+  }
+
 }
